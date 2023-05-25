5f056295f64b1c34e418fdebc3fe97cd2a413ef4
EngAliMAhdi <eng.aliosama.mahdi@gmail.com>
2023-05-25 21:34:22 +0300

    QuizApp

--- firstapp/src/Quiz.js
+++ firstapp/src/Quiz.js
@@ -1,5 +1,9 @@
 import React, { useState } from 'react'
 import { dataList } from './DataList'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle'
+import './Quiz.css'
+
 
 //console.log(dataList[0].answers[2].isCorrect)
 function Quiz() {
@@ -21,25 +25,25 @@ function Quiz() {
        console.log(score)
      }
   return (
-    <div>
+    <div className='w-25 m-auto p-4 bg-secondary text-warning mt-5'>
         <h1>Quiz App</h1>
         {
             check ? (
                 <>
                 <p>your score is {score}/{dataList.length}</p>
-                <button onClick={()=>window.location.reload()}>Try Again</button>
+                <button className='d-block w-50 m-auto btn btn-danger' onClick={()=>window.location.reload()}>Try Again</button>
                 </>
             ) :
             (
                 <>
-                <p>{dataList[count].question}</p>
+                <p>Q({count+1}/{dataList.length})  {dataList[count].question}</p>
         {
             dataList[count].answers.map(
-                (item,index) => <button onClick={()=>checkAnswer(item.isCorrect)} key={index} >{item.ans}</button>
+                (item,index) => <button className='d-block w-50 m-auto btn btn-warning mb-1' onClick={()=>checkAnswer(item.isCorrect)} key={index} >{item.ans}</button>
             )
        }
         <br/>
-        <button onClick={nextQuestion}>Next</button>
+        <button className='d-block w-50 m-auto btn btn-primary mb-1' onClick={nextQuestion}>Next</button>
                 </>
             )
         }
