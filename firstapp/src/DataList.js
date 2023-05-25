2caadedc100dd40a51879ed4dfba029f387091e1
EngAliMAhdi <eng.aliosama.mahdi@gmail.com>
2023-05-25 21:12:27 +0300

    QuizApp

---
+++ firstapp/src/DataList.js
@@ -0,0 +1,30 @@
+export const dataList=[
+    {
+        question:"What is the image tag ?",
+        answers:[
+            {ans:"<a>", isCorrect:false},
+            {ans:"<h1>", isCorrect:false},
+            {ans:"<img>", isCorrect:true},
+            {ans:"<br>", isCorrect:false},
+        ]
+    },
+    {
+        question:"What is the heading tag ?",
+        answers:[
+            {ans:"<a>", isCorrect:false},
+            {ans:"<h1>", isCorrect:true},
+            {ans:"<img>", isCorrect:false},
+            {ans:"<br>", isCorrect:false},
+        ]
+    }
+    ,
+    {
+        question:"What is the Link tag ?",
+        answers:[
+            {ans:"<a>", isCorrect:true},
+            {ans:"<h1>", isCorrect:false},
+            {ans:"<img>", isCorrect:false},
+            {ans:"<br>", isCorrect:false},
+        ]
+    }
+]
---
+++ firstapp/src/Quiz.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import { dataList } from './DataList'
+
+//console.log(dataList[0].answers[2].isCorrect)
+function Quiz() {
+    const [count,setCount] = useState(0)
+    const [score,setScore] = useState(0)
+    const [check,setCheck] = useState(false)
+    function nextQuestion(){
+       if(count<dataList.length-1)
+       {
+        setCount(count+1)
+       }else{
+        setCheck(true)
+       }
+    }
+    function checkAnswer(c){
+       if(c){
+           setScore(score+1) 
+       }
+       console.log(score)
+     }
+  return (
+    <div>
+        <h1>Quiz App</h1>
+        {
+            check ? (
+                <>
+                <p>your score is {score}/{dataList.length}</p>
+                <button onClick={()=>window.location.reload()}>Try Again</button>
+                </>
+            ) :
+            (
+                <>
+                <p>{dataList[count].question}</p>
+        {
+            dataList[count].answers.map(
+                (item,index) => <button onClick={()=>checkAnswer(item.isCorrect)} key={index} >{item.ans}</button>
+            )
+       }
+        <br/>
+        <button onClick={nextQuestion}>Next</button>
+                </>
+            )
+        }
+        
+    </div>
+  )
+}
+
+export default Quiz
